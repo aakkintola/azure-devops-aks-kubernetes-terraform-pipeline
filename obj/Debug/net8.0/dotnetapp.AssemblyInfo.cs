15a296b604e9161d72b2804bc7d1f372171f1f7e
Akintola Marke <akintolamarke@gmail.com>
2024-04-02 09:21:46 -0400

    Adding dotnet build elements

--- obj/Debug/net8.0/dotnetapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7682b24740f8afde9dc24106ed13887111053431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fab417b607e11f76ffbb76bd52aaf049c6c476")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
